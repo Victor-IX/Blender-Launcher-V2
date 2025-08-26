a01fd92f0d235f802bc0ea3cce8e739e4239612a
Victor <victor.ched@gmail.com>
2025-08-26 19:52:42 -0400

    #278 Fix download issue for bforartis build

--- source/threads/downloader.py
+++ source/threads/downloader.py
@@ -1,14 +1,18 @@
+import base64
 import logging
+
 from dataclasses import dataclass
 from pathlib import Path
+from PySide6.QtCore import Signal
+from urllib3.exceptions import MaxRetryError
 
 from modules._copyfileobj import copyfileobj
 from modules.connection_manager import REQUEST_MANAGER
 from modules.enums import MessageType
 from modules.settings import get_library_folder
+from modules.string_utils import extract_filename_from_url
 from modules.task import Task
-from PySide6.QtCore import Signal
-from urllib3.exceptions import MaxRetryError
+from threads.scraper import BFA_NC_WEBDAV_SHARE_TOKEN
 
 logger = logging.getLogger()
 
@@ -24,15 +28,21 @@ class DownloadTask(Task):
         self.progress.emit(0, 0)
         temp_folder = Path(get_library_folder()) / ".temp"
         temp_folder.mkdir(exist_ok=True)
-        dist = temp_folder / Path(self.link).name
+        filename = extract_filename_from_url(self.link)
+        dist = temp_folder / filename
+        headers = {}
+
+        if "cloud.bforartists.de/public.php/webdav" in self.link:
+            auth_string = base64.b64encode(f"{BFA_NC_WEBDAV_SHARE_TOKEN}:".encode()).decode("ascii")
+            headers["Authorization"] = f"Basic {auth_string}"
 
         try:
-            with self.manager.request("GET", self.link, preload_content=False, timeout=10) as r:
+            with self.manager.request("GET", self.link, preload_content=False, timeout=10, headers=headers) as r:
                 self._download(r, dist)
         except MaxRetryError as e:
             logger.exception(f"Requesting is taking longer than usual! {e}")
             self.message.emit("Requesting is taking longer than usual! see debug logs for more.", MessageType.ERROR)
-            with self.manager.request("GET", self.link, preload_content=False) as r:
+            with self.manager.request("GET", self.link, preload_content=False, headers=headers) as r:
                 self._download(r, dist)
 
         self.finished.emit(dist)
