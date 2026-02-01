cfb6fc52e93319227042ca6fdf2b59953a7ad750
zeptofine <xpsychonauticonx@gmail.com>
2026-02-01 01:01:49 -0500

    Attempt to detect the user's locale on startup

--- source/utils/i18n_init.py
+++ source/utils/i18n_init.py
@@ -1,3 +1,5 @@
+import locale
+import os
 import sys
 from pathlib import Path
 
@@ -9,4 +11,23 @@ else:
     LOCALIZATION_PATH = Path("source/localization").resolve()
 
 i18n.load_path.append(LOCALIZATION_PATH)
+
+
+# determine the language the user is using
+loc: str
+if sys.platform == "win32":
+    import ctypes
+
+    windll = ctypes.windll.kernel32
+    loc = locale.windows_locale[windll.GetUserDefaultUILanguage()]
+elif (x := os.environ.get("LANG")) is not None:
+    loc = x
+elif (x := locale.getlocale()[0]) is not None:
+    loc = x
+else:
+    loc = "en_US"
+
+loc = loc.split("_", 1)[0]
+
+i18n.set("locale", loc)
 i18n.set("fallback", "en")
