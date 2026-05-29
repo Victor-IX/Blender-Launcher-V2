c2b328788cafe843708535c027244bbb461ed773
zeptofine <xpsychonauticonx@gmail.com>
2026-05-29 09:27:03 -0400

    feat: introduce container modifications

---
+++ source/modules/container_detect.py
@@ -0,0 +1,10 @@
+import os
+from pathlib import Path
+
+IS_FLATPAK = (
+    bool(os.getenv("container"))  # noqa: SIM112 (flatpak 'container' env variable is lowercase -~-)
+    or Path("/.flatpak-info").exists()
+)
+
+
+IS_CONTAINED = IS_FLATPAK # expand if necessary
