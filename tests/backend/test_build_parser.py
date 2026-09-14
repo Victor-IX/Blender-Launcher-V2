582252aa0b8dc33319fef729bbef07ae635f71e4
neurodiverseEsoteric <577091+neurodiverseEsoteric@users.noreply.github.com>
2026-09-14 15:48:28 -0700

    Update test_build_parser.py

--- tests/backend/test_build_parser.py
+++ tests/backend/test_build_parser.py
@@ -13,7 +13,7 @@ from source.modules.build_info import (
     parse_blender_ver,
 )
 from source.modules.platform_utils import get_platform
-from source.modules.settings import get_bash_arguments, set_bash_arguments
+from source.modules.settings import get_bash_arguments, set_bash_arguments, get_use_nohup_button, set_use_nohup_button
 from tests.config import SKIP_TESTS_THAT_MODIFY_CONFIG
 
 
@@ -64,13 +64,17 @@ def test_get_args():
 
     if idx == 1:
         bargs = get_bash_arguments()
+        nohupArgs = get_use_nohup_button()
+        bstr = 'nohup "/blender/blender" '
+        if not nohupArgs:
+            bstr='"/blender/blender"'
         set_bash_arguments("")
-
+        set_use_nohup_button(False)
     x = [
         (
             get_args(info=info),
             [win_root + "/blender/blender.exe"],
-            'nohup "/blender/blender" ',
+            bstr,
             "open -W -n /blender/Blender/Blender.app --args",
         ),
         (
@@ -82,25 +86,25 @@ def test_get_args():
         (
             get_args(info=info, exe="bforartists.exe"),
             ["cmd", "/C", win_root + "/blender/bforartists.exe"],
-            'nohup "/blender/blender" ',
+            bstr,
             "open -W -n /blender/Blender/Blender.app --args",
         ),
         (
             get_args(info=info_c),
             [win_root + "/blender/bforartists"],
-            'nohup "/blender/bforartists" ',
+            bstr,
             "open -W -n /blender/Blender/Blender.app --args",
         ),
         (
             get_args(info=info, launch_mode=LaunchOpenLast()),
             [win_root + "/blender/blender.exe", "--open-last"],
-            'nohup "/blender/blender"  --open-last',
+            bstr + " --open-last",
             "open -W -n /blender/Blender/Blender.app --args --open-last",
         ),
         (
             get_args(info=info, launch_mode=LaunchWithBlendFile(Path(root) / "file.blend")),
             [win_root + "/blender/blender.exe", win_root + "/file.blend"],
-            'nohup "/blender/blender"  "/file.blend"',
+            bstr + "/file.blend",
             'open -W -n /blender/Blender/Blender.app --args --open-last "/file.blend"',
         ),
     ]
