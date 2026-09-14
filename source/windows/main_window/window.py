4abe86599a5b49669e9fb33f778e6aa5c6039859
neurodiverseEsoteric <577091+neurodiverseEsoteric@users.noreply.github.com>
2026-09-14 16:49:31 -0700

    Update window.py

--- source/windows/main_window/window.py
+++ source/windows/main_window/window.py
@@ -68,6 +68,7 @@ from modules.settings import (
     get_window_geometry,
     get_window_maximized,
     get_worker_thread_count,
+    get_use_nohup_button,
     is_library_folder_valid,
     set_dont_show_resource_warning,
     set_library_folder,
@@ -524,7 +525,10 @@ class BlenderLauncher(BaseWindow):
             _popen([dist.as_posix(), "--instanced", "update", self.latest_tag], no_console=False)
         elif self.platform == "Linux":
             os.chmod(dist.as_posix(), 0o744)
-            _popen(f'nohup "{dist.as_posix()}" --instanced update {self.latest_tag}')
+            if get_use_nohup_button():
+                _popen(f'nohup "{dist.as_posix()}" --instanced update {self.latest_tag}')
+            else:
+                _popen(f'"{dist.as_posix()}" --instanced update {self.latest_tag}')
 
         # Destroy currently running Blender Launcher instance
         self.quit_()
@@ -997,7 +1001,10 @@ class BlenderLauncher(BaseWindow):
         elif self.platform == "Linux":
             exe = (cwd / "Blender Launcher").as_posix()
             os.chmod(exe, 0o744)
-            _popen('nohup "' + exe + '" -instanced')
+            if get_use_nohup_button():
+                _popen('nohup "' + exe + '" -instanced')
+            else:
+                _popen(exe + '" - instanced')
         elif self.platform == "macOS":
             # sys.executable should be something like /.../Blender Launcher.app/Contents/MacOS/Blender Launcher
             app = Path(sys.executable).parent.parent.parent
