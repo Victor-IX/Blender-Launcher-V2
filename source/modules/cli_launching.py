956ad1039365f59f49f68186067ac984409fa8ec
neurodiverseEsoteric <577091+neurodiverseEsoteric@users.noreply.github.com>
2026-09-21 11:56:51 -0700

    Update cli_launching.py

--- source/modules/cli_launching.py
+++ source/modules/cli_launching.py
@@ -111,7 +111,7 @@ def cli_launch(
         build = matches[0]
         build_info = basics[build]
 
-    args = get_args(build_info, launch_mode=launch_mode, linux_nohup=get_use_nohup())
+    args = get_args(build_info, launch_mode=launch_mode, linux_nohup=False)
     
     if isinstance(args, list):
         args.extend(blender_args)
